f11c9e9041ec064a4aa1233ee2bd7cad68349c66
TimTheHero <117226730+TimTheHero@users.noreply.github.com>
2023-03-18 23:23:03 +0100

    Add files via upload

---
+++ Converter.py
@@ -0,0 +1,41 @@
+import PySimpleGUI as sg
+import random as rdm
+
+themes = ['Black', 'BlueMono', 'BluePurple', 'BrightColors', 'BrownBlue', 'Dark', 'Dark2', 'DarkAmber', 'DarkBlack', 'DarkBlack1', 'DarkBlue', 'DarkBlue1', 'DarkBlue10', 'DarkBlue11', 'DarkBlue12', 'DarkBlue13', 'DarkBlue14', 'DarkBlue15', 'DarkBlue16', 'DarkBlue17', 'DarkBlue2', 'DarkBlue3', 'DarkBlue4', 'DarkBlue5', 'DarkBlue6', 'DarkBlue7', 'DarkBlue8', 'DarkBlue9', 'DarkBrown', 'DarkBrown1', 'DarkBrown2', 'DarkBrown3', 'DarkBrown4', 'DarkBrown5', 'DarkBrown6', 'DarkBrown7', 'DarkGreen', 'DarkGreen1', 'DarkGreen2', 'DarkGreen3', 'DarkGreen4', 'DarkGreen5', 'DarkGreen6', 'DarkGreen7', 'DarkGrey', 'DarkGrey1', 'DarkGrey10', 'DarkGrey11', 'DarkGrey12', 'DarkGrey13', 'DarkGrey14', 'DarkGrey15', 'DarkGrey2', 'DarkGrey3', 'DarkGrey4', 'DarkGrey5', 'DarkGrey6', 'DarkGrey7', 'DarkGrey8', 'DarkGrey9', 'DarkPurple', 'DarkPurple1', 'DarkPurple2', 'DarkPurple3', 'DarkPurple4', 'DarkPurple5', 'DarkPurple6', 'DarkPurple7', 'DarkRed', 'DarkRed1', 'DarkRed2', 'DarkTanBlue', 'DarkTeal', 'DarkTeal1', 'DarkTeal10', 'DarkTeal11', 'DarkTeal12', 'DarkTeal2', 'DarkTeal3', 'DarkTeal4', 'DarkTeal5', 'DarkTeal6', 'DarkTeal7', 'DarkTeal8', 'DarkTeal9', 'Default', 'Default1', 'DefaultNoMoreNagging', 'GrayGrayGray', 'Green', 'GreenMono', 'GreenTan', 'HotDogStand', 'Kayak', 'LightBlue', 'LightBlue1', 'LightBlue2', 'LightBlue3', 'LightBlue4', 'LightBlue5', 'LightBlue6', 'LightBlue7', 'LightBrown', 'LightBrown1', 'LightBrown10', 'LightBrown11', 'LightBrown12', 'LightBrown13', 'LightBrown2', 'LightBrown3', 'LightBrown4', 'LightBrown5', 'LightBrown6', 'LightBrown7', 'LightBrown8', 'LightBrown9', 'LightGray1', 'LightGreen', 'LightGreen1', 'LightGreen10', 'LightGreen2', 'LightGreen3', 'LightGreen4', 'LightGreen5', 'LightGreen6', 'LightGreen7', 'LightGreen8', 'LightGreen9', 'LightGrey', 'LightGrey1', 'LightGrey2', 'LightGrey3', 'LightGrey4', 'LightGrey5', 'LightGrey6', 'LightPurple', 'LightTeal', 'LightYellow', 'Material1', 'Material2', 'NeutralBlue', 'Purple', 'Python', 'PythonPlus', 'Reddit', 'Reds', 'SandyBeach', 'SystemDefault', 'SystemDefault1', 'SystemDefaultForReal', 'Tan', 'TanBlue', 'TealMono', 'Topanga']
+sg.theme(themes[rdm.randint(0,len(themes)-1)])
+layout = [
+	[
+		sg.Input(key = '-INPUT-'),
+		sg.Spin(['km to mile','kg to pound','sec to min'], key = '-UNITS-'),
+		sg.Button('Convert', key = '-CONVERT-')
+	],
+	[sg.Text('Output', key = '-OUTPUT-')]
+]
+
+window = sg.Window('Converter',layout)
+
+while True:
+	event, values = window.read()
+
+	if event == sg.WIN_CLOSED:
+		break
+
+	if event == '-CONVERT-':
+		input_value = values['-INPUT-']
+		if input_value.isnumeric():
+			match values['-UNITS-']:
+				case 'km to mile':
+					output = round(float(input_value) * 0.6214,2)
+					output_string = f'{input_value} km are {output} miles.'
+				case 'kg to pound':
+					output = round(float(input_value) * 2.20462,2)
+					output_string = f'{input_value} kg are {output} pounds.'
+				case 'sec to min':
+					output = round(float(input_value) / 60,2)
+					output_string = f'{input_value} seconds are {output} minutes.'
+
+			window['-OUTPUT-'].update(output_string)
+		else:
+			window['-OUTPUT-'].update('Please enter a number')
+
+window.close()
